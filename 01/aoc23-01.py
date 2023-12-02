70d8b10e3d83b3757781601e263435f34edb549d
Thomas E. Hansen <teh6@st-andrews.ac.uk>
2023-12-02 18:11:31 +0100

    [ 01 ] Part 2 -_-
    Part 2 coming out of nowhere to wreck things...

--- 01/aoc23-01.py
+++ 01/aoc23-01.py
@@ -12,11 +12,44 @@ else:
 
 # PART-1 #
 
-sum = 0
+def get_digits(line: str):
+  return [c for c in line if c.isdigit()]
+
+
+# PART-2 #
+
+lit_nums = { "one":   "o1ne"
+           , "two":   "tw2o"
+           , "three": "thr3ee"
+           , "four":  "fo4ur"
+           , "five":  "fi5ve"
+           , "six":   "s6x"
+           , "seven": "se7en"
+           , "eight": "eig8ht"
+           , "nine":  "ni9ne"
+           , "zero":  "zer0o"
+           }
+
+def lit_to_digits(line: str):
+  for lit, num in lit_nums.items():
+    line = line.replace(lit, num)
+  return line
+
+
+# SOLUTION #
+
+sum1 = 0
+sum2 = 0
 for line in input:
   # extract the digits
-  digits = [c for c in line if c.isdigit()]
+  digits = get_digits(line)
   # add the relevant parts to the sum
-  sum += int(digits[0]) * 10 + int(digits[-1])
+  sum1 += int(digits[0]) * 10 + int(digits[-1])
 
-print(f"Part 1: {sum}")
+  digits = get_digits(lit_to_digits(line))
+  print(digits)
+  sum2 += int(digits[0]) * 10 + int(digits[-1])
+
+
+print(f"Part 1: {sum1}")
+print(f"Part 2: {sum2}")
