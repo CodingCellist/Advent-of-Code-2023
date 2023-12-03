fa67b6ca19e2d7a3ffed55d8e3456c9e4b47a2d7
Thomas E. Hansen <teh6@st-andrews.ac.uk>
2023-12-03 19:02:54 +0100

    [ 03 ] New idea

--- 03/aoc23-03.py
+++ 03/aoc23-03.py
@@ -12,7 +12,7 @@ else:
 
 # PART-1 #
 
-# a symbol point is a character which is not `.` or a digit, along with its
+# a symbol-point is a character which is not `.` or a digit, along with its
 # position in the line
 def symbol_points(line: str):
   return [ (i, line[i])
@@ -20,61 +20,47 @@ def symbol_points(line: str):
            if not line[i].isdigit() and line[i] != '.'
            ]
 
-# get a line's numbers along with their position in the line
-def number_points(line: str):
-  no_pts = []
-  number = None
-  pts = []
-  for i in range(len(line)):
-    c = line[i]
-    if c.isdigit() and number is None:
-      number = str(c)
-      pts.append(i)
-    elif c.isdigit() and number is not None:
-      number += str(c)
-      pts.append(i)
-    # c is no longer a digit, and we've accumulated some number in str-form
-    elif number is not None:
-      # add the new number along with its digit-points...
-      no_pts.append(
-        (pts, int(number))
-      )
-      # ... and then
 
 # oh god, 2D grid navigation, noooo!
 
+# idea: Store a list of numbers in a list along with their positions,
+#       then remove from the list as we find things relative to the
+#       symbol-points.
+#       I _believe_ that should prevent counting duplicates...
 
 
 # SOLUTION #
 
-
+# for testing
 input = [ "467..114.."
         , "...*......"
-        , ".35..633."
-        , ".....#..."
-        , "17*......"
-        , "....+.58."
-        , ".592....."
-        , ".....755."
-        , "..$.*...."
-        , "664.598.."
+        , "..35..633."
+        , "......#..."
+        , "617*......"
+        , ".....+.58."
+        , "..592....."
+        , "......755."
+        , "...$.*...."
+        , ".664.598.."
         ]
 
-# there's probably a different way to do this, but this is easy
-line_no = 0
+### # there's probably a different way to do this, but this is easy
+### line_no = 0
+input_array = []
 
-prev_line = None
-next_line = None
-has_symbols = False
+### has_symbols = False
 
+# forget about streaming the file, I want the whole 2D array
 for line in input:
 
   sym_pts = symbol_points(line)
-  # if we found a symbol, we need to read the next line to check for
-  # surrounding part numbers
-  has_symbols = len(sym_pts) != 0
 
-  # final steps in loop
-  if has_symbols:
-    prev_line = line
-  line_no += 1
+  # store the line
+  input_array.append(line)
+
+  ### # if we found a symbol, we need to read the next line to check for
+  ### # surrounding part numbers
+  ### has_symbols = len(sym_pts) != 0
+  ###
+  ### line_no += 1
+
