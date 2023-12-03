bfa29e9423a85d72d78ccfd9518b9acc5ea7c2a4
Thomas E. Hansen <teh6@st-andrews.ac.uk>
2023-12-03 18:06:14 +0100

    [ 03 ] False start
    Committing because... idk, just to see my mistakes?

--- 03/aoc23-03.py
+++ 03/aoc23-03.py
@@ -20,6 +20,26 @@ def symbol_points(line: str):
            if not line[i].isdigit() and line[i] != '.'
            ]
 
+# get a line's numbers along with their position in the line
+def number_points(line: str):
+  no_pts = []
+  number = None
+  pts = []
+  for i in range(len(line)):
+    c = line[i]
+    if c.isdigit() and number is None:
+      number = str(c)
+      pts.append(i)
+    elif c.isdigit() and number is not None:
+      number += str(c)
+      pts.append(i)
+    # c is no longer a digit, and we've accumulated some number in str-form
+    elif number is not None:
+      # add the new number along with its digit-points...
+      no_pts.append(
+        (pts, int(number))
+      )
+      # ... and then
 
 # oh god, 2D grid navigation, noooo!
 
@@ -40,5 +60,21 @@ input = [ "467..114.."
         , "664.598.."
         ]
 
+# there's probably a different way to do this, but this is easy
+line_no = 0
+
+prev_line = None
+next_line = None
+has_symbols = False
+
 for line in input:
-  print(symbol_points(line))
+
+  sym_pts = symbol_points(line)
+  # if we found a symbol, we need to read the next line to check for
+  # surrounding part numbers
+  has_symbols = len(sym_pts) != 0
+
+  # final steps in loop
+  if has_symbols:
+    prev_line = line
+  line_no += 1
